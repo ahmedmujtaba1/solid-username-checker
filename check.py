23c0938c060a462bda78f557f397650378665bac
ahmedmujtaba1 <ahmed.mujtaba11737@hhs.edu.pk>
2023-10-09 22:25:48 +0500

    All Done

--- check.py
+++ check.py
@@ -1,19 +1,20 @@
 import requests, random
 
 def check_existence(email:str) -> bool:
-    url = f"https://api.apilayer.com/email_verification/check?email={email}"
+    if email.endswith('@yahoo.com'):
+        r = requests.get(f"https://api.validemail.net/?email={email}&token=54d0bb46f65a4c0d9894454021f14560")
+        re = r.json()
+        additional = re["EmailAdditionalInfo"]
+        flag = False
+        if len(additional) > 1:
+            flag = True
+        return flag
+    else:
+        r = requests.get(f"https://apps.emaillistverify.com/api/verifEmail?secret=EWPfWcivWhA1T31WAJGfg&email={email}")
+        flag = False
+        if "ok" in str(r.content):
+            flag = True
 
-    payload = {}
-    api_keys = ['JvsouKusHXQGhLZ2A6Ukh93jFYAdrjqs',"C4XTOR0gK7531jTKBdpGJtGYkJh5x54B","5tAckRNOPmkRUkudQm07DRG8PKcYB3tm"]
-    headers= {
-        "apikey": random.choice(api_keys)
-    }
-
-    response = requests.request("GET", url, headers=headers, data = payload)
-
-    status_code = response.status_code
-    result = response.json()
-    # print("Results : ", result)
-    return result['mx_found']
-
-# print(check_existence('ahmedkdjkddodikolk@mail.com'))
+        return flag
+    
+# print(check_existence("amedmujatavaisjsk@yahoo.com"))
